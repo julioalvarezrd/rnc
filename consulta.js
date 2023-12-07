fa7ec8c30dd87cbe4a98d750344bdc0523e622e8
Julio Alvarez <julioalvarezrd@gmail.com>
2023-12-07 17:53:30 -0400

    Primer Commit

---
+++ consulta.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Realizar la consulta al archivo DGI.txt
+    fetch('DGI.txt')
+        .then(response => response.text())
+        .then(data => {
+            // Procesar los datos del archivo
+            const lines = data.split('\n');
+            lines.forEach(line => {
+                const [rnc, nombre] = line.split('|');
+                // Puedes hacer lo que quieras con los datos (por ejemplo, mostrarlos en la consola)
+                console.log(`RNC: ${rnc}, Nombre: ${nombre}`);
+            });
+        })
+        .catch(error => console.error('Error al cargar el archivo DGI.txt:', error));
+});
